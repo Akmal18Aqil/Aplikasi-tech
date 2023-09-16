6249d65916947393a760458096ca746c22e69856
Akmal <akmalaqilwahyu@gmail.com>
2023-09-17 06:14:43 +0700

    Revisi bagian item

--- app/src/main/java/com/akmal/media/ListHeroAdapter.kt
+++ app/src/main/java/com/akmal/media/ListHeroAdapter.kt
@@ -22,7 +22,7 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adap
 
     override fun getItemCount(): Int = listHero.size
     class ListViewHolder (itemView : View): RecyclerView.ViewHolder(itemView) {
-        val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+        val imgPhoto: ImageView = itemView.findViewById(R.id.gambar)
         val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
         val tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
 
